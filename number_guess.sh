260c72a019373c4a5221cbfce15f820b60db53fe
Jason Farinelli <jason.farinelli@gmail.com>
2024-06-14 03:06:25 +0000

    feat: username prompt and welcome message.

--- number_guess.sh
+++ number_guess.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+PSQL="psql -X --username=freecodecamp --dbname=number_game --tuples-only -c"
+
 NUMBER=$(( RANDOM % 1000 + 1)) 
-echo $NUMBER
+
+echo 'Enter your username:'
+read USERNAME
+
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
+
+#If user data present, print welcome back info
+if [[ ! -z $USER_ID ]]
+then
+  GAME_DATA=$($PSQL "SELECT COUNT(game_id), MIN(number_of_guesses) FROM games WHERE user_id = $USER_ID")
+  echo $GAME_DATA | while read NUMBER_OF_GAMES BAR BEST_SCORE
+  do
+    echo "Welcome back, $USERNAME! You have played $NUMBER_OF_GAMES games, and your best game took $BEST_SCORE guesses."
+  done
+#If no user data present, print welcome info
+else
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+fi
